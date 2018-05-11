3f75086ad311f60c09a83c6cbbc0783aa9101d18
maddyking44 <madelineking44@gmail.com>
2018-05-11 16:49:08 +1200

    api json not reading as text

--- GHNewBehaviourScript.cs
+++ GHNewBehaviourScript.cs
@@ -1,21 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
     // Rigidbody rb = GetComponent<Rigidbody>(); <- import component inyo rb variable
 	// Barbarian.animation.Mean
 
 public class NewBehaviourScript : MonoBehaviour {
 	//this is a string variable defined in unity in the inspector pane. public means you can see it in the unity inspector
-
+// public string url = "http://api.wunderground.com/api/d5a02b585b94a99a/conditions/q/New_Zealand/Wellington.json";
 	public Animator ani;
 	public string myName;
 	// Use this for initialization, ie componentDidMount. Debug.Log is console.log (Window > console)
 	void Start () {
 		Debug.Log("Here I am ");
+		StartCoroutine(GetWeather());
+	}
+	IEnumerator GetWeather() {
+		UnityWebRequest www = UnityWebRequest.Get("http://api.wunderground.com/api/d5a02b585b94a99a/conditions/q/New_Zealand/Wellington.json");
+		// using (WWW www = new WWW(url))
+        // {
+            yield return www.SendWebRequest();
+ 
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+ 
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+            // Renderer renderer = GetComponent<Renderer>();
+            // renderer.material.mainTexture = www.texture;
+        // }
+
 		ani = GetComponent<Animator>();
 	}
-	    public float distancePerSecond;
 
 
 	// Update is called once per frame    
@@ -32,17 +54,5 @@ public class NewBehaviourScript : MonoBehaviour {
   		if (Input.GetKeyDown(KeyCode.W)) {
   	ani.SetTrigger("Run");
   }
-        // ani.Play("RoundKick");
     }
 }
-
-//  var animator:Animator;
- 
-//      function Start(){
-//      animator=GetComponent(Animator);
-//      }
-//      function Update()
-//      {
-//              if (Input.GetKeyDown() {
-//                      animator.Play("NAME OF ANIMATION");
-//      }
